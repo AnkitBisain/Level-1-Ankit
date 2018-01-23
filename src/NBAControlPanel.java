a63d6f2bec86b2091a56348a4c666fb7f3ff8e73
AnkitBisain <league@WTS-IM-6.attlocal.net>
2018-01-22 18:50:38 -0800

    Working on NBA scoreboard/controlpanel

--- src/NBAControlPanel.java
+++ src/NBAControlPanel.java
@@ -1,4 +1,75 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class NBAControlPanel {
-
+	JFrame frame;
+	JPanel mainPanel;
+	JPanel homeChange;
+	JPanel awayChange;
+	JPanel homeScoreChange;
+	JPanel homeTOChange;
+	JPanel homeFoulsChange;
+	JPanel awayScoreChange;
+	JPanel awayTOChange;
+	JPanel awayFoulsChange;
+	JLabel homeTitle;
+	JLabel homeScoreTitle;
+	JLabel homeTOTitle;
+	JLabel homeFoulsTitle;
+	JLabel awayTitle;
+	JLabel awayScoreTitle;
+	JLabel awayTOTitle;
+	JLabel awayFoulsTitle;
+	JButton homeScore1;
+	JButton homeScore2;
+	JButton homeScore3;
+	JButton homeFouls1;
+	JButton homeTO1;
+	JButton awayScore1;
+	JButton awayScore2;
+	JButton awayScore3;
+	JButton awayFouls1;
+	JButton awayTO1;
+	
+	
+	public static void main(String[] args) {
+		NBAControlPanel p = new NBAControlPanel();
+	}
+	
+	public NBAControlPanel(){
+		frame = new JFrame();;
+		mainPanel = new JPanel();
+		homeChange = new JPanel();
+		awayChange = new JPanel();
+		homeScoreChange = new JPanel();
+		homeTOChange = new JPanel();
+		homeFoulsChange = new JPanel();
+		awayScoreChange = new JPanel();
+		awayTOChange = new JPanel();
+		awayFoulsChange = new JPanel();
+		homeTitle = new JLabel("Home");
+		homeScoreTitle = new JLabel("Score");
+		homeTOTitle = new JLabel("TOs");
+		homeFoulsTitle = new JLabel("Fouls");
+		awayTitle = new JLabel("Away");
+		awayScoreTitle = new JLabel("Score");
+		awayTOTitle = new JLabel("TOs");
+		awayFoulsTitle = new JLabel("Fouls");
+		JButton homeScore1;
+		JButton homeScore2;
+		JButton homeScore3;
+		JButton homeFouls1;
+		JButton homeTO1;
+		JButton awayScore1;
+		JButton awayScore2;
+		JButton awayScore3;
+		JButton awayFouls1;
+		JButton awayTO1;
+	}
+	
 }
--- src/NBAScoreboard.java
+++ src/NBAScoreboard.java
@@ -1,3 +1,7 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,6 +19,8 @@ public class NBAScoreboard {
 	JPanel homePanel;
 	JPanel awayPanel;
 	JPanel mainPanel;
+	JPanel homeStuff;
+	JPanel awayStuff;
 	JLabel hTOText;
 	JLabel hTO;
 	JLabel aTOText;
@@ -23,10 +29,10 @@ public class NBAScoreboard {
 	JLabel hF;
 	JLabel aFouls;
 	JLabel aF;
-	JLabel homeTitle;
-	JLabel hScore;
-	JLabel awayTitle;
-	JLabel aScore;
+	JButton homeTitle;
+	JButton hScore;
+	JButton awayTitle;
+	JButton aScore;
 	
 	public NBAScoreboard() {
 		this.homeName = "HOME";
@@ -34,12 +40,14 @@ public class NBAScoreboard {
 		this.homeScore = 0;
 		this.awayScore = 0;
 		this.homeFouls = 0;
-		this.awayFouls = 9;
+		this.awayFouls = 0;
 		this.homeTO = 3;
 		this.awayTO = 3;
 		frame = new JFrame();
 		homePanel = new JPanel();
+		homeStuff = new JPanel();
 		awayPanel = new JPanel();
+		awayStuff = new JPanel();
 		mainPanel = new JPanel();
 		hTOText = new JLabel("TIMEOUTS");
 		hTO = new JLabel(Integer.toString(homeTO));
@@ -49,34 +57,63 @@ public class NBAScoreboard {
 		hF = new JLabel(Integer.toString(homeFouls));
 		aFouls = new JLabel("FOULS");
 		aF = new JLabel(Integer.toString(awayFouls));
-		homeTitle = new JLabel("HOME");
-		hScore = new JLabel(Integer.toString(homeScore));
-		awayTitle = new JLabel("AWAY");
-		aScore = new JLabel(Integer.toString(awayScore));
+		homeTitle = new JButton("HOME");
+		hScore = new JButton(Integer.toString(homeScore));
+		awayTitle = new JButton("AWAY");
+		aScore = new JButton(Integer.toString(awayScore));
+		hScore.setFont(new Font("Arial", Font.PLAIN, 40));
+		aScore.setFont(new Font("Arial", Font.PLAIN, 40));
 		run();
 	}
 
 	void run() {
-		homePanel.add(homeTitle);
-		homePanel.add(hScore);
-		homePanel.add(hTOText);
-		homePanel.add(hTO);
-		homePanel.add(hFouls);
-		homePanel.add(hF);
-		awayPanel.add(awayTitle);
-		awayPanel.add(aScore);
-		awayPanel.add(aTOText);
-		awayPanel.add(aTO);
-		awayPanel.add(aFouls);
-		awayPanel.add(aF);
-		mainPanel.add(homePanel);
-		mainPanel.add(awayPanel);		
+		homePanel.setLayout(new BorderLayout());
+		homePanel.add(homeTitle, BorderLayout.NORTH);
+		homePanel.add(hScore, BorderLayout.CENTER);
+		homeStuff.add(hTOText);
+		homeStuff.add(hTO);
+		homeStuff.add(hFouls);
+		homeStuff.add(hF);
+		homePanel.add(homeStuff, BorderLayout.SOUTH);
+		awayPanel.setLayout(new BorderLayout());
+		awayPanel.add(awayTitle, BorderLayout.NORTH);
+		awayPanel.add(aScore, BorderLayout.CENTER);
+		awayStuff.add(aTOText);
+		awayStuff.add(aTO);
+		awayStuff.add(aFouls);
+		awayStuff.add(aF);
+		awayPanel.add(awayStuff, BorderLayout.SOUTH);
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(homePanel, BorderLayout.WEST);
+		mainPanel.add(awayPanel, BorderLayout.EAST);
 		frame.add(mainPanel);
 		frame.pack();
-		frame.setVisible(true);		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
 	}
 	
-	public static void main(String[] args) {
-		NBAScoreboard s = new NBAScoreboard();
+	public void changeHomeScore(int i){
+		this.homeScore+=i;
+		hScore.setText(Integer.toString(homeScore));
+	}
+	public void changeHomeTO(int i){
+		this.homeTO+=i;
+		hScore.setText(Integer.toString(homeScore));
+	}
+	public void changeHomeFouls(int i){
+		this.homeFouls+=i;
+		hF.setText(Integer.toString(homeFouls));
+	}
+	public void changeawayScore(int i){
+		this.awayScore+=i;
+		aScore.setText(Integer.toString(awayScore));
+	}
+	public void changeawayTO(int i){
+		this.awayTO+=i;
+		aScore.setText(Integer.toString(awayScore));
+	}
+	public void changeawayFouls(int i){
+		this.awayFouls+=i;
+		aF.setText(Integer.toString(awayFouls));
 	}
 }
